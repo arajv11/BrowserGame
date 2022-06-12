6b35373da04c97c9b984b902306cfe4605845c65
Anish Raj <a.rajv11@gmail.com>
2022-06-12 11:05:46 -0400

    third commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # BrowserGame
-# Needs a score or win/lose
-# Game where you watch animation(like balls moving around a field) and test your memory of the objects and colors
+# Single player
+# Score points by memorizing the shapes and answering questions
--- script.js
+++ script.js
@@ -4,26 +4,46 @@ let remainingTime = 10;
 
 
 let start = document.getElementById("start");
+// let colors = ["red", "orange", "yellow", "green", "blue", "purple", "black"]
 let colors = ["red", "black"]
-let index = 0;
+let red = 0
+let black = 0
+let emptyArray = []
 console.log(start)
+
 
 start.addEventListener("click", showImage);
 
 function showImage() {
     console.log("Game started")
-    let gameArea = document.getElementById("div2")
-    console.log(gameArea)
-    let shape = document.createElement('div')
-    shape.classList.add("circle")
-    if (index % 2 == 0) {
-        shape.style.background = "red"
-        console.log(index)
-    } else {
-        shape.style.background = "black"
-        console.log(index)
+    for (let i = 1; i <= 10; i++) {
+        let gameArea = document.getElementById("div2")
+        console.log(gameArea)
+        let shape = document.createElement('div')
+        shape.classList.add("circle")
+        let color = colors[Math.floor( Math.random() * colors.length )];
+        shape.style.background = color
+        switch (color) {
+            case "red":
+                red++;
+                break;
+            case "black":
+                black++;
+                break;
+        }
+        gameArea.append(shape)
     }
-    index +=1
-    console.log(index)
-    gameArea.append(shape)
+
+    setTimeout(function () {
+        console.log("red="+red)
+        console.log("black="+black)
+        let circleList = document.getElementsByClassName("circle")
+        while (circleList.length > 0) {
+            circleList[circleList[0].remove()]
+        }
+    }, 3000)
+}
+
+function deleteImage() {
+
 }
